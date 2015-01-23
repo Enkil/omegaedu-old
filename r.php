342bd08cc2d8f63fee2ed61ed79cc5290c1dedd2
Igor Timohin <itimohin@NET-SIMPLE.RU>
2015-01-23 09:38:23 +0300

    --

--- r.php
+++ r.php
@@ -1,5 +1,5 @@
 <?
 
-mail("tia@net-simple.ru", "Клиент (ЕГЭ, 8 месяцев, /s1)", "Вам пришла заявка.<br><br>Имя: ".$_POST['name']."<br>Телефон: ".$_POST['tel']."<br><br>Источник: ".$_POST['source']."<br>Форма: ".$_POST['form']."", "Content-type: text/html; charset=utf-8\r\nFrom:Омега <no-reply@omegaedu.ru>");
+mail("tia@net-simple.ru", "Клиент (ЕГЭ, 8 месяцев, /s1)", "Вам пришла заявка.<br><br>Имя: ".$_POST['name']."<br>Телефон: ".$_POST['tel']."<br><br>Источник: ".$_POST['source']."<br>Форма: ".$_POST['form']."<br>Email: ".$_POST['mail']."", "Content-type: text/html; charset=utf-8\r\nFrom:Омега <no-reply@omegaedu.ru>");
 
 ?>
