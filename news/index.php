9147f301bd7c8f3e1ab6abadab4092f39a8cc394
Igor Timohin <itimohin@NET-SIMPLE.RU>
2015-01-26 05:47:45 +0300

    rework news

--- news/index.php
+++ news/index.php
@@ -9,6 +9,115 @@
     <br/>
 
 
+    <!-- News list -->
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-1.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>Поздравляем с успешной сдачей!</h2>
+            <p class="small">30.06.14</p>
+            <p class="text-muted">
+                Поздравляем выпускников с успешной сдачей всех ЕГЭ! Всё оказалось не так страшно, верно? Настало время окончательно определиться с выбором вуза, подать документы и сменить свой социальный статус «школьник» на «студент»
+            </p>
+        </div>
+    </div>
+
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-2.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>Внимание набор в группы подготовки к ЕГЭ</h2>
+            <p class="small">02.04.2014</p>
+            <p class="text-muted">
+                Внимание! Набор во все группы подготовки к ЕГЭ 2014 закрыт. Открыт приём заявок в группы для подготовки к ЕГЭ 2015. Заключившим договор до 1 августа 2014 приятные скидки!
+            </p>
+        </div>
+    </div>
+
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-3.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>Открыты новые группы</h2>
+            <p class="small">29.01.2014</p>
+            <p class="text-muted">
+                Внимание! Центр наш подготовки к ЕГЭ открывает новые группы подготовки к ЕГЭ по русскому языку и математике. Занятия начинаются в феврале. Программа подготовки «ускоренных» групп рассчитана на 4 месяца. Занятия Будут проводиться раз в неделю по 4 академических часа. Стоимость обучения составит 4500 р. за месяц. Преподаватели: подготовка к ЕГЭ по математике — Дмитрий Вячеславович Амелин, подготовка к ЕГЭ по русскому языку — Елена Сергеевна Дьякова. (Отзывы)
+                Количество мест в группах ограничено, в каждую группу набираем до 10 человек. Звоните или оставляйте заявку.
+            </p>
+        </div>
+    </div>
+
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-4.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>Поздравляем всех с наступающим Новым Годом!</h2>
+            <p class="small">30.12.2013</p>
+            <p class="text-muted">
+                Поздравляем всех с наступающим Новым Годом! Пусть в грядущем 2014 году все поставленные задачи будут решены, все планы реализованы, все цели достигнуты! Напоминаем, что мы отдыхаем с 30.12.2014 до 13.12.2014. Желаем всем учащимся, преподавателям и сочувствующим хорошего отдыха и прекрасных выходных!
+            </p>
+        </div>
+    </div>
+
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-5.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>Открыты новые группы подготовки к ЕГЭ по русскому языку, математике и обществознанию.</h2>
+            <p class="small">19.11.2013</p>
+            <p class="text-muted">
+                Внимание! Открыты новые группы подготовки к ЕГЭ по русскому языку, математике и обществознанию. Занятия по русскому языку проводит преподаватель академической гимназии при СПбГУ Евгений Витальевич Кулешов по понедельникам в 19:00 в ауд. №2. Занятия по математике проводит профессор кафедры методики обучения математики факультета математики РГПУ им. А. И. Герцена доктор педагогических наук эксперт ЕГЭ Владимир Викторович Орлов по вторникам в 19:00 в ауд. №2. Занятия по обществознанию проводит преподаватель академической гимназии при СПбГУ эксперт ЕГЭ Людмила Васильевна Влащенко по вторникам в 19:00 в ауд. №4. Звоните или оставляйте заявку.
+            </p>
+        </div>
+    </div>
+
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-6.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>С Днем Учителя!</h2>
+            <p class="small">05.10.2013</p>
+            <p class="text-muted">
+                Поздравляем лучших преподавателей Санкт-Петребурга с Днём Учителя! Спасибо Вам за Вашу работу!
+            </p>
+        </div>
+    </div>
+
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-6.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>Образовательная выставке «Высшее образование для ваших детей»</h2>
+            <p class="small">16.09.2013</p>
+            <p class="text-muted">
+                Наш центр подготовки к ЕГЭ принял участие образовательной выставке «Высшее образование для ваших детей». Спасибо всем пришедшим! Мы были рады видеть Вас у нашего стенда! Надеемся, что мы ответили на все Ваши вопросы. Если нет, то звоните нам по телефону или приходите с 16:30 до 21:30 к нам на курсы по адресу наб. реки Фонтанки, д. 55
+            </p>
+        </div>
+    </div>
+
+    <div class="row top-margin vertical-align">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
+            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-8.jpg" alt=""/>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <h2>Поздравляем всех учащихся с Днём Знаний!</h2>
+            <p class="small">01.09.2013</p>
+            <p class="text-muted">
+                Наш центр подготовки к ЕГЭ поздравляет всех учащихся с Днём Знаний! Мы желаем всем успешной учёбы, увлеченности и уверенности в собственных силах во время подготовки к ЕГЭ! Тем, кто решит готовиться с нами, мы дарим скидку 20% на первый месяц обучения. Оставляйте заявки на сайте или звоните нам. Предложение действует до 15-го сентября. Готовьтесь рационально!
+            </p>
+        </div>
+    </div>
+
+
+
+
     <!-- Pagination -->
 <!--    <div class="row">-->
 <!--        <nav class="text-center">-->
@@ -31,137 +140,5 @@
 <!--            </ul>-->
 <!--        </nav>-->
 <!--    </div>-->
-
-
-    <!-- News list -->
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block media-middle" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block media-middle" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block media-middle" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-    <div class="row top-margin vertical-align">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-block text-center">
-            <img class="img-responsive img-rounded text-center center-block media-middle" src="/img/news/news-1.jpg" alt=""/>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-            <h2><a href="#">Изменение расписания</a></h2>
-            <p>Уважаемые учащиеся! Хотим сообщить, что у нас обновилось расписание занятий. </p>
-            <p class="text-muted">
-                Мы продолжаем набор в группы по подготовке к ЕГЭ-2015 и ГИА-2015 по всем предметам. Для записи на бесплатное пробное занятие достаточно оставить заявку на сайте или позвонить по телефону. Торопитесь, количество мест в группах ограничено. Готовьтесь к ЕГЭ с нами — готовьтесь РАЦИОнально!
-            </p>
-        </div>
-    </div>
-
-
-
-
-    <!-- Pagination -->
-    <div class="row">
-        <nav class="text-center">
-            <ul class="pagination">
-                <li>
-                    <a href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                </li>
-                <li class="active"><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li>
-                    <a href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </div>
 
 <?php include('../footer.php'); ?>
