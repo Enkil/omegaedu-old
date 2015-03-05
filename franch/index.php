91f183264c070a23683fc619b77edc244bee77be
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-05 12:07:13 +0300

    franch

--- franch/index.php
+++ franch/index.php
@@ -1,5 +1,372 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<title>Omega</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/fonts.css" />
+</head>
+<body>
+	<div class="wrapper">
 
-$activepage='franch'; include('../header.php');
+		<div class="block1">
+			<div class="left">
+				<p class="logo_p"><img class="logo" src="img/omega-logo.png" alt="" /></p>
+				<p class="quote">Наша программа франчайзинга&nbsp;&ndash; ваша прибыль с&nbsp;первого месяца работы!</p>
+			</div>
+			<div class="right">
+				<p class="scheme">
+					<span class="ege">Центр подготовки<br />к ЕГЭ и ГИА</span>
+				</p>
+			</div>
+		</div>
 
-?>
+		<div class="block2">
+			<div class="left">
+				<h4>Почему пришло время открыть учебный центр по&nbsp;подготовке к&nbsp;ЕГЭ и&nbsp;ГИА?</h4>
+				<p class="logo_p"><img class="logo_mini" src="img/logo-mini.jpg" alt="" /></p>
+			</div>
+			<div class="right">
+				<div class="all_items">
+					<img class="items" src="img/items.png" alt="" />
+					<div class="itemd1">
+						<p class="item1">Родители вкладывают в&nbsp;образование детей&nbsp;&ndash; заработайте на&nbsp;этом, ведь спрос на&nbsp;услуги есть <span>всегда</span> в&nbsp;каждом городе!</p>
+					</div>
+					<div class="itemd2">
+						<p class="item2">Рынок учебных услуг по&nbsp;подготовке к&nbsp;ЕГЭ и&nbsp;ГИА почти свободен. Сетевые проекты по&nbsp;подготовке только начинают развиваваться, <span>станьте первым!</span></p>
+					</div>
+					<div class="itemd3">
+						<p class="item3">Получайте прибыль с&nbsp;первого месяца работы&nbsp;&mdash; все, что Вам нужно это <span>желание</span> начать!</p>
+					</div>
+					<p class="basis">Так почему Вам выгодно работать именно с&nbsp;центром подготовки &laquo;Омега&raquo;?<br/> &mdash;&nbsp;<b>узнайте в&nbsp;следующих разделах!</b></p>
+				</div>
+			</div>
+		</div>
+
+		<div class="block_form">
+			<div class="forms">
+			<p class="application">Откройте свой бизнес вместе с нами,<br />занимайтесь любимым делом и получайте высокий доход!</p>
+				<form method="" action="">
+					<p><label><input class="name" type="text" name="name" placeholder="Имя" required=""></label></p>
+					<p><label><input class="phone" type="text" name="phone" data-masked-input="+7 (999) 999-99-99" required="" placeholder="Телефон" autocomplete="on"></label></p>
+					<p><label><input class="email" type="email" name="email" placeholder="E-mail" required="" autocomplete="on"></label></p>
+					<p><input class="submit" type="submit" value="Оставить заявку"></p>
+				</form>
+			</div>
+		</div>
+
+		<div class="block3">
+			<div class="left">
+				<div class="biss_plans">
+				<!-- 1 -->
+					<div class="biss_plan">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan1.png" alt="" /></p>
+						<p class="biss_p">Аналитика Вашего региона и <span>адекватный</span> персональный бизнес-план</p>
+					</div>
+				<!-- 2 -->
+					<div class="biss_plan">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan2.png" alt="" /></p>
+						<p class="biss_p"><span>CRM </span> система - Вы сможете контро&shy;ли&shy;ровать всех своих клиентов, их оплаты и&nbsp;видеть показатели своего бизнеса</p>
+					</div>
+				<!-- 3 -->
+					<div class="biss_plan biss_float">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan3.png" alt="" /></p>
+						<p class="biss_p">Сайт с «высококон&shy;вер&shy;тируемым лендин&shy;гом» - целевая (рек&shy;ламная) страница сайта выполнена с учетом психологии потреби&shy;те&shy;ля и позво&shy;ляет конвертировать обычные переходы на сайт в <span>реальных клиентов!</span></p>
+					</div>
+				<!-- 4 -->
+					<div class="biss_plan">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan4.png" alt="" /></p>
+						<p class="biss_p">Профессиональный медиа-план &ndash; Вы <span>легко</span> стартуете в&nbsp;своем городе, основываясь на нашем опыте</p>
+					</div>
+				<!-- 5 -->
+					<div class="biss_plan">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan5.png" alt="" /></p>
+						<p class="biss_p">Все наши <span>«ноу-хау»</span> - мы делимся опытом по нетривиальным способам привле&shy;чения клиентов и организации собственных образо&shy;вательных мероприятий (презентации, олимпиады и др.)</p>
+					</div>
+				<!-- 6 -->
+					<div class="biss_plan biss_float">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan6.png" alt="" /></p>
+						<p class="biss_p"><span>Исчерпывающий</span> набор документов по&nbsp;организации бизнеса: от маркетингового руководства по управ&shy;лению персоналом, до&nbsp;пошаговой инструкции «как зарегистрировать компанию»</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="right">
+				<h4>Центр подготовки к&nbsp;ЕГЭ и&nbsp;ГИА &laquo;Омега&raquo;&nbsp;&mdash; это не&nbsp;только все, что нужно для сдачи экзаменов, но&nbsp;и&nbsp;все для старта <b>Вашего бизнеса!</b></h4>
+				<p class="logo_p"><img class="logo_mini" src="img/logo-mini.jpg" alt="" /></p>
+			</div>
+		</div>
+
+		<div class="block4">
+			<div class="left">
+				<h2>Что в итоге?</h2>
+				<p class="logo_p"><img class="logo_mini" src="img/logo-mini.jpg" alt="" /></p>
+			</div>
+			<div class="right">
+				<img class="shelves_img" src="img/shelves.png" alt="" />
+				<div class="shelves_block">
+					<p class="shelves_p">Вы&nbsp;знаете <b>все</b> о&nbsp;том, что необходимо для открытия, все секреты привлечения клиентов, поиска персонала, работы с&nbsp;клиентами и&nbsp;стандарты. Вы&nbsp;знаете <b>ВЕСЬ</b> бизнес от А&nbsp;до&nbsp;Я!</p>
+				</div>
+				<div class="shelves_block">
+					<p class="shelves_p">Вы&nbsp;получаете готовые рекламные материалы, <nobr>медиа-план</nobr> созданный для <b>ВАШЕГО</b> города и, главное, <b>РАБОТАЮЩИЙ</b> сайт, с&nbsp;грамотно настроенной страницей продаж курсов.</p>
+				</div>
+				<div class="shelves_block">
+					<p class="shelves_p">Вы&nbsp;получаете систему учета и управления клиентами, находящуюся в&nbsp;облаке и&nbsp;позволяющую контролировать <b>ВАШ</b> бизнес в&nbsp;любой точке мира или страны.</p>
+				</div>
+				<p class="basis">Мы&nbsp;существуем 2 года, но&nbsp;за&nbsp;это время разработали уникальный формат, необходимый для&nbsp;старта. Мы&nbsp;делимся опытом и&nbsp;мы&nbsp;<b>гарантируем,</b> что открытие учебного центра по&nbsp;подготовке к&nbsp;ЕГЭ и&nbsp;ГИА это <b>лучшая инвестиция в&nbsp;свое будущее и&nbsp;будущее выпускников!</b></p>
+			</div>
+		</div>
+
+		<div class="block_form">
+			<div class="forms">
+			<p class="application">Откройте свой бизнес вместе с нами,<br />занимайтесь любимым делом и получайте высокий доход!</p>
+				<form method="" action="">
+					<p><label><input class="name" type="text" name="name" placeholder="Имя" required=""></label></p>
+					<p><label><input class="phone" type="text" name="phone" data-masked-input="+7 (999) 999-99-99" required="" placeholder="Телефон" autocomplete="on"></label></p>
+					<p><label><input class="email" type="email" name="email" placeholder="E-mail" required="" autocomplete="on"></label></p>
+					<p><input class="submit" type="submit" value="Оставить заявку"></p>
+				</form>
+			</div>
+		</div>
+
+		<div class="block5">
+			<div class="left">
+				<div class="menu">
+					<h4>Возможности</h4>
+					<p class="item_programm">
+						<span class="active">ОМЕГА</span><br />
+						<span>ОМЕГА консалтинг</span><br />
+						<span>ОМЕГА +</span>
+					</p>
+				</div>
+			</div>
+			<div class="right">
+				<p class="basis">
+					Мы подготовили для Вас несколько решений.<br />
+					Какое подходит именно Вам?!<br />
+					Подскажем, чтобы могли определиться, но не теряйте время!<br />
+					<b>Успейте занять</b> уникальную нишу в своем городе!
+				</p>
+				<p class="logo_var1"><img src="img/logo_var1.png" alt="" /></p>
+			</div>
+			<div class="left_new">
+				<h3>Омега</h3>
+				<p class="programm">Для тех, кто не хочет ждать и готов открыться и получать прибыль прямо сейчас!</p>
+				<div class="programm_info">
+					<div class="programm_text">
+						<img class="li-img" src="img/li.png" alt="" />
+						<div class="text1">Помещение</div>
+						<div class="text2">Почасовая аренда переговорных комнат</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li.png" alt="" />
+						<div class="text1">Интвестиции</div>
+						<div class="text2">от 150 000 р.</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li.png" alt="" />
+						<div class="text1">Время</div>
+						<div class="text2">от подписания договора до открытия 2 недели!</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li.png" alt="" />
+						<div class="text1">Стоимость пакета</div>
+						<div class="text2">150 000 р.</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li.png" alt="" />
+						<div class="text3">Риски</div>
+						<div class="text4">Минимальные риски. Вы&nbsp;не&nbsp;вкладываете деньги в&nbsp;ремонт помещения и&nbsp;закупку дорогого оборудования, Вы&nbsp;начинаете бизнес ЗДЕСЬ и&nbsp;СЕЙЧАС!</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li.png" alt="" />
+						<div class="text3">* Скидки</div>
+
+						<div class="text4">* Скидки, при условии подписания договора.<br />С&nbsp;момента обращения за&nbsp;10 дней&nbsp;&mdash; 10%. Если обратитесь прямо сейчас&nbsp;&mdash; Вы заплатите всего <b>135&nbsp;000 р. за&nbsp;полный пакет!</b></div>
+					</div>
+				</div>
+			</div>
+			<div class="right_new">
+				<p class="li2"><img src="img/li2.png" alt="" /></p>
+				<h3>Доход:</h3>
+				<p class="many">1 000 000 - 3 000 000</p>
+				<p class="li2"><img src="img/li2.png" alt="" /></p>
+				<h3>Прибыль:</h3>
+				<p class="many">300 000 - 900 000</p>
+				<p class="li3"><img src="img/li3.png" alt="" /></p>
+				<p class="agitation">Сделайте свой выбор&nbsp;&mdash; откройте свой бизнес с&nbsp;минимальным риском и&nbsp;высоким доходом!</p>
+			</div>
+		</div>
+
+		<div class="block6">
+			<div class="left">
+				<div class="menu">
+					<h4>Возможности</h4>
+					<p class="item_programm">
+						<span>ОМЕГА +</span><br />
+						<span class="active">ОМЕГА консалтинг</span><br />
+						<span>ОМЕГА</span>
+					</p>
+				</div>
+			</div>
+			<div class="right">
+				<p class="basis">
+					Мы подготовили для Вас несколько решений.<br />
+					Какое подходит именно Вам?!<br />
+					Подскажем, чтобы могли определиться, но не теряйте время!<br />
+					<b>Успейте занять</b> уникальную нишу в своем городе!
+				</p>
+				<p class="logo_var1"><img src="img/logo_var1.png" alt="" /></p>
+			</div>
+			<div class="left_new">
+				<h3>Омега <span>консалтинг</span></h3>
+				<p class="programm">Наши специалисты уже имеют опыт открытия серии учебных центров и их количество постоянно растет.</p>
+				<p class="programm">Мы представим Вам:</p>
+				<div class="biss_plans">
+				<!-- 1 -->
+					<div class="biss_plan">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan6.png" alt="" /></p>
+						<p class="biss_p">Полный пакет документов, необходимых для открытия</p>
+					</div>
+				<!-- 2 -->
+					<div class="biss_plan">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan2.png" alt="" /></p>
+						<p class="biss_p">CRM систему управления клиентами</p>
+					</div>
+				<!-- 3 -->
+					<div class="biss_plan biss_float">
+						<img class="check" src="img/check.png" alt="" />
+						<p class="biss_imgp"><img class="img-biss" src="img/biss-plan4.png" alt="" /></p>
+						<p class="biss_p">Предоставим рекоммендации по рекламе и маркетингу</p>
+					</div>
+					<p class="ps">* Сайт, фирменный стиль, методическая поддержка и права на бренд не входят в консалтинговый пакет.</p>
+				</div>
+			</div>
+			<div class="right_new">
+				<p class="li2"><img src="img/li2.png" alt="" /></p>
+				<h3>Стоимость пакета</h3>
+				<p class="many">89 000 р.</p>
+				
+				<p class="li3"><img src="img/li3.png" alt="" /></p>
+				<p class="agitation">Сделайте свой выбор&nbsp;&mdash; откройте свой бизнес с&nbsp;минимальным риском и&nbsp;высоким доходом!</p>
+			</div>
+		</div>
+
+		<div class="block7">
+			<div class="left">
+				<div class="menu">
+					<h4>Возможности</h4>
+					<p class="item_programm">
+						<span>ОМЕГА</span><br />
+						<span>ОМЕГА консалтинг</span><br />
+						<span class="active">ОМЕГА +</span>
+					</p>
+				</div>
+			</div>
+			<div class="right">
+				<p class="basis">
+					Мы подготовили для Вас несколько решений.<br />
+					Какое подходит именно Вам?!<br />
+					Подскажем, чтобы могли определиться, но не теряйте время!<br />
+					<b>Успейте занять</b> уникальную нишу в своем городе!
+				</p>
+				<p class="logo_var1"><img src="img/logo_var1.png" alt="" /></p>
+			</div>
+			<div class="left_new">
+				<h3>Омега +</h3>
+				<p class="programm">Открытие собственного флагманского образовательного центра. Серьезный шаг, предполагает инвестиции в проект</p>
+				<div class="programm_info">
+					<div class="programm_text">
+						<img class="li-img" src="img/li4.png" alt="" />
+						<div class="text1">Помещение</div>
+						<div class="text2">50 - 120 м<sup>2</sup></div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li4.png" alt="" />
+						<div class="text1">Интвестиции</div>
+						<div class="text2">900 000 - 1 300 000</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li4.png" alt="" />
+						<div class="text1">Время</div>
+						<div class="text2">от подписания договора до открытия 1,5-2 месяца!</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li4.png" alt="" />
+						<div class="text1">Стоимость пакета</div>
+						<div class="text2">230 000 - 290 000 (город с населением от 1 млн.ч.)</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li4.png" alt="" />
+						<div class="text3">Риски</div>
+						<div class="text4">Вы можете приобрести нашу франшизу в любое время, но открыться мы рекомендуем <b>строго</b> до 1 ноября</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li4.png" alt="" />
+						<div class="text3">Дополнительно</div>
+						<div class="text4">Дизайн интерьеров, смета на ремонт помещения и закупку оборудования. Дополнительные инструкции для персонала</div>
+					</div>
+					<div class="programm_text">
+						<img class="li-img" src="img/li4.png" alt="" />
+						<div class="text5">Бонус</div>
+						<div class="text6">Работа персонального менеджера на всех этапах запуска проекта</div>
+					</div>
+				</div>
+			</div>
+			<div class="right_new">
+				<p class="li2"><img src="img/li2.png" alt="" /></p>
+				<h3>Доход:</h3>
+				<p class="many">1 000 000 - 3 000 000</p>
+				<p class="li2"><img src="img/li2.png" alt="" /></p>
+				<h3>Прибыль:</h3>
+				<p class="many">300 000 - 900 000</p>
+				<p class="li3"><img src="img/li3.png" alt="" /></p>
+				<p class="agitation">Сделайте свой выбор&nbsp;&mdash; откройте свой бизнес с&nbsp;минимальным риском и&nbsp;высоким доходом!</p>
+			</div>
+		</div>
+
+		<div class="block_form">
+			<div class="forms">
+			<p class="application">Откройте свой бизнес вместе с нами,<br />занимайтесь любимым делом и получайте высокий доход!</p>
+				<form method="" action="">
+					<p><label><input class="name" type="text" name="name" placeholder="Имя" required=""></label></p>
+					<p><label><input class="phone" type="text" name="phone" data-masked-input="+7 (999) 999-99-99" required="" placeholder="Телефон" autocomplete="on"></label></p>
+					<p><label><input class="email" type="email" name="email" placeholder="E-mail" required="" autocomplete="on"></label></p>
+					<p><input class="submit" type="submit" value="Оставить заявку"></p>
+				</form>
+			</div>
+		</div>
+
+		<div class="block8">
+			<div class="left">
+				<p class="logo_p"><img class="logo" src="img/omega-logo.png" alt="" /></p>
+			</div>
+			<div class="right">
+				<p class="contacts">Контакты</p>
+				<p class="contacts_img"><img src="img/tel.png" alt="" /></p>
+				<p class="contacts_p"><span>ЗВОНИТЕ</span><br />
+				+7 (911) 096-90-36</p>
+				<p class="contacts_img"><img src="img/mail.png" alt="" /></p>
+				<p class="contacts_p"><span>ПИШИТЕ</span><br />
+				omegaedu@gmail.com</p>
+				<p class="contacts_img"><img src="img/www.png" alt="" /></p>
+				<p class="contacts_p other"><span>ЗАХОДИТЕ НА САЙТ</span><br />
+				omegaedu.ru/franch<br/>
+				omegaedu.ru</p>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
