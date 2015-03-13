3b4bb65d33ac1c7c0a7d34840ccab8b8c2f9ab4d
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-13 12:55:40 +0300

    fix js

--- footer.php
+++ footer.php
@@ -42,11 +42,8 @@
 
 </div>
 
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
