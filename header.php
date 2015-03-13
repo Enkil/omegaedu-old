7348bc02e4ac4a972b646e57654f76e182bb77b8
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-13 05:10:34 +0300

    dropdown cities

--- header.php
+++ header.php
@@ -2,97 +2,111 @@
 <html>
 <head>
 
-	<link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.css"/>
-	<link rel="stylesheet" href="/bower_components/components-font-awesome/css/font-awesome.min.css"/>
-	<script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.css"/>
+    <link rel="stylesheet" href="/bower_components/components-font-awesome/css/font-awesome.min.css"/>
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
 
-	<title>Омега | Образовательный проект | Развитие через обучение</title>
-	
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="/img/favicon.png"/>
-	
-	<meta property="og:title" content="Омега | Образовательный проект | Развитие через обучение"/>
-	<meta property="og:url" content="http://OmegaEdu.ru"/>
-	<meta property="og:description" content="Курсы подготовки к ЕГЭ по всем предметам с профессиональными преподавателями из МГУ и МФТИ."/>
-	<meta name="description" content="Курсы подготовки к ЕГЭ по всем предметам с профессиональными преподавателями из МГУ и МФТИ."/>
-	<meta property="og:image" content=""/>
-	<meta property="og:site_name" content="Омега | Образовательный проект | Развитие через обучение"/>
+    <title>Омега | Образовательный проект | Развитие через обучение</title>
 
-	<script type="text/javascript" src="http://vk.com/js/api/openapi.js?98"></script>
-	<script type="text/javascript">
-	  VK.init({apiId: 3779244, onlyWidgets: true});
-	</script>
-	
-	<style>
-		body{font-family:"Open Sans"; font-size:15px; color:#000;}
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/img/favicon.png"/>
 
-		hr{border-top: 1px solid #bbb;}
+    <meta property="og:title" content="Омега | Образовательный проект | Развитие через обучение"/>
+    <meta property="og:url" content="http://OmegaEdu.ru"/>
+    <meta property="og:description" content="Курсы подготовки к ЕГЭ по всем предметам с профессиональными преподавателями из МГУ и МФТИ."/>
+    <meta name="description" content="Курсы подготовки к ЕГЭ по всем предметам с профессиональными преподавателями из МГУ и МФТИ."/>
+    <meta property="og:image" content=""/>
+    <meta property="og:site_name" content="Омега | Образовательный проект | Развитие через обучение"/>
 
-		a:hover {font-weight:normal; text-decoration:none; border-bottom:1px solid #005580;}
-		a.icon:hover {font-weight:normal; text-decoration:none;}
-		a.image:hover {text-decoration:none; border-bottom:none;}
-		a.black {color:#000;}
-		a.black:hover {color:#000; text-decoration:none; border-bottom:none;}
+    <script type="text/javascript" src="http://vk.com/js/api/openapi.js?98"></script>
+    <script type="text/javascript">
+        VK.init({apiId: 3779244, onlyWidgets: true});
+    </script>
 
-		.topmenuup {margin-top:16px; margin-left:18px;}
-		.topmenuup a {font-size:21px;}
-		.topmenuup a:hover {font-weight:normal; text-decoration:none; border-bottom:1px solid #005580;}
-		.topmenuup a.active {border-bottom:1px solid #08c;}
-		.topmenuup a.active:hover {border-bottom:1px solid #005580;}
-		.topmenuup div {display:inline-block; text-align:left;}
-		.topmenudown {margin-top:18px; margin-left:18px;}
-		.topmenudown a {font-size:15px;}
-		.topmenudown a:hover {font-weight:normal; text-decoration:none; border-bottom:1px solid #005580;}
-		.topmenudown div {display:inline-block; text-align:left;}
-		.topmenudown a.active {border-bottom:1px solid #08c;}
-		.topmenudown a.active:hover {border-bottom:1px solid #005580;}
+    <style>
+        body{font-family:"Open Sans"; font-size:15px; color:#000;}
 
-		.bttn {display: inline-block; padding:4px 12px 6px 12px; line-height:20px; background-color:#08c; color:#fff; border-radius:4px;}
-		.bttn:hover {border:none; color:#fff; background-color:#005580; cursor:pointer;}
+        hr{border-top: 1px solid #bbb;}
 
-		input[type="text"] {border:1px solid #bbb; color:#444;}
-		input[type="text"]:hover {border:1px solid #08c;}
-		input[type="text"]:focus {border:1px solid #08c;}
+        a:hover {font-weight:normal; text-decoration:none; border-bottom:1px solid #005580;}
+        a.icon:hover {font-weight:normal; text-decoration:none;}
+        a.image:hover {text-decoration:none; border-bottom:none;}
+        a.black {color:#000;}
+        a.black:hover {color:#000; text-decoration:none; border-bottom:none;}
 
-	</style>
+        .topmenuup {margin-top:16px; margin-left:18px;}
+        .topmenuup a {font-size:21px;}
+        .topmenuup a:hover {font-weight:normal; text-decoration:none; border-bottom:1px solid #005580;}
+        .topmenuup a.active {border-bottom:1px solid #08c;}
+        .topmenuup a.active:hover {border-bottom:1px solid #005580;}
+        .topmenuup div {display:inline-block; text-align:left;}
+        .topmenudown {margin-top:18px; margin-left:18px;}
+        .topmenudown a {font-size:15px;}
+        .topmenudown a:hover {font-weight:normal; text-decoration:none; border-bottom:1px solid #005580;}
+        .topmenudown div {display:inline-block; text-align:left;}
+        .topmenudown a.active {border-bottom:1px solid #08c;}
+        .topmenudown a.active:hover {border-bottom:1px solid #005580;}
+
+        .bttn {display: inline-block; padding:4px 12px 6px 12px; line-height:20px; background-color:#08c; color:#fff; border-radius:4px;}
+        .bttn:hover {border:none; color:#fff; background-color:#005580; cursor:pointer;}
+
+        input[type="text"] {border:1px solid #bbb; color:#444;}
+        input[type="text"]:hover {border:1px solid #08c;}
+        input[type="text"]:focus {border:1px solid #08c;}
+
+    </style>
 
 </head>
-<body> 
-	<div class="container" id="content">
-		<div class="row" style="margin-top:20px; margin-bottom:25px;">
+<body>
+<div class="container" id="content">
+    <div class="row" style="margin-top:20px; margin-bottom:25px;">
 
-			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<a class="image" href="/"><img class="img-responsive" src="/img/logo_3.png" style="width:175px; margin-left:15px;" /></a>
-				<p style="font-weight:normal; font-size:13px; line-height:16px; margin-top:0px; margin-bottom:0px; margin-left:15px; color:#777;">Подготовка к вузам Москвы</p>
-			</div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <a class="image" href="/"><img class="img-responsive" src="/img/logo_3.png" style="width:175px; margin-left:15px;" /></a>
+            <p style="font-weight:normal; font-size:13px; line-height:16px; margin-top:0px; margin-bottom:0px; margin-left:15px; color:#777;">Подготовка к вузам Москвы</p>
+        </div>
 
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-				<div class="topmenuup">
-					<div><a target="_blank" href="http://s1.omegaedu.ru/index.php?utm_source=omegaedu">Курсы ЕГЭ / ГИА</a></div>
-					<div style="margin-left:45px;"><a <? if($activepage=='price') echo('class="active"'); ?> href="/price/">Цены</a></div>
-					<div style="margin-left:45px;"><a <? if($activepage=='franch') echo('class="active"'); ?> href="/franch/">Франчайзинг</a></div>
-				</div>
-				<div class="topmenudown">
-					<div><a <? if($activepage=='news') echo('class="active"'); ?> href="/news/">Новости</a></div>
-					<div style="margin-left:38px;"><a <? if($activepage=='timetable') echo('class="active"'); ?> href="/timetable/">Расписание</a></div>
-					<div style="margin-left:38px;"><a <? if($activepage=='teachers') echo('class="active"'); ?> href="/teachers/">Преподаватели</a></div>
-					<div style="margin-left:38px;"><a <? if($activepage=='contacts') echo('class="active"'); ?> href="/contacts/">Контакты</a></div>
-				</div>
-			</div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="topmenuup">
+                <div><a target="_blank" href="http://s1.omegaedu.ru/index.php?utm_source=omegaedu">Курсы ЕГЭ / ГИА</a></div>
+                <div style="margin-left:45px;"><a <? if($activepage=='price') echo('class="active"'); ?> href="/price/">Цены</a></div>
+                <div style="margin-left:45px;"><a <? if($activepage=='franch') echo('class="active"'); ?> href="/franch/">Франчайзинг</a></div>
+            </div>
+            <div class="topmenudown">
+                <div><a <? if($activepage=='news') echo('class="active"'); ?> href="/news/">Новости</a></div>
+                <div style="margin-left:38px;"><a <? if($activepage=='timetable') echo('class="active"'); ?> href="/timetable/">Расписание</a></div>
+                <div style="margin-left:38px;"><a <? if($activepage=='teachers') echo('class="active"'); ?> href="/teachers/">Преподаватели</a></div>
+                <div style="margin-left:38px;"><a <? if($activepage=='contacts') echo('class="active"'); ?> href="/contacts/">Контакты</a></div>
+            </div>
+        </div>
 
-			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center">
-				<p class="col-lg-12 col-md-12 col-sm-6 col-xs-6" style="margin-top:16px; margin-bottom:0px; font-size:21px;">
-					8 (495) 134 45 17
-				</p>
-				<p class="col-lg-12 col-md-12 col-sm-6 col-xs-6" style="margin-top:13px; margin-bottom:0px; font-size:15px;">
-					Мы в соц. сетях:
-					<a href="http://vk.com/omegaedu" target="_blank" style="font-size:28px; border-bottom:none; margin-left:5px;">
-						<i class="fa fa-vk"></i></a><a href="http://youtube.com/channel/UCZmxeBd4XBfRcLooiW1nISw" target="_blank" style="font-size:28px; border-bottom:none; margin-left:5px;">
-						<i class="fa fa-youtube"></i>
-					</a>
-				</p>
-			</div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-right">
 
-		</div>
-	
+            <div class="dropdown" style="margin-top: 16px;">
+                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true" style="width: 100%">
+                    Выберите город
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="http://omegaedu.ru">Москва</a></li>
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="http://khv.omegaedu.ru">Хабаровск</a></li>
+                </ul>
+            </div>
+
+            <p style="font-size:21px; line-height: 20px; margin-bottom: 5px; margin-top: 5px;">
+                8 (4212) 60-70-73
+            </p>
+
+            <p style="font-size:15px; line-height: 20px;">
+                Мы в соц. сетях:
+                <a href="http://vk.com/omegaedu" target="_blank" style="font-size:28px; border-bottom:none; margin-left:5px;">
+                    <i class="fa fa-vk"></i></a><a href="http://youtube.com/channel/UCZmxeBd4XBfRcLooiW1nISw" target="_blank" style="font-size:28px; border-bottom:none; margin-left:5px;">
+                    <i class="fa fa-youtube"></i>
+                </a>
+            </p>
+
+        </div>
+
+    </div>
+
