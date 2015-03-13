541c2839aaad3adfb05e3c630935b6ae65828902
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-13 04:04:18 +0300

    php mailer

--- franch/formAction.php
+++ franch/formAction.php
@@ -1,5 +1,4 @@
 <?php
-require_once('config.php');
 
 echo
     htmlspecialchars($_POST['name']) . "\r\n" .
@@ -13,6 +12,8 @@ $message =
     htmlspecialchars($_POST['phone']) . "\r\n" .
     htmlspecialchars($_POST['email']);
 $message = wordwrap($message, 70, "\r\n");
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
 $headers =
     'From:' . $adminEmail . "\r\n" .
     'Reply-To:' . $replyEmail . "\r\n" .
@@ -24,5 +25,37 @@ if  (mail($to, $subject, $message, $headers)) {
     echo 'Sending error';
 };
 
+$siteName = 'OmegaEDU';
+$adminEmail = 'no-reply@omegaedu.ru';
+$replyEmail = 'timohin.i@gmail.com';
+$managerEmail = 'timohin.i@gmail.com';
+
+require 'PHPMailer/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Set who the message is to be sent from
+$mail->setFrom($adminEmail, 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo($replyEmail, 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress($managerEmail, 'OmegaEDU Franchize Manager');
+//Set the subject line
+$mail->Subject = 'PHPMailer mail() test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
 
 header("Location: ".$_SERVER['HTTP_REFERER']);
+
