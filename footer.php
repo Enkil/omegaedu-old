2931c50feb34ee11ce2ed7ddc9bd1224afef7a7d
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-03-13 12:50:11 +0300

    fix js

--- footer.php
+++ footer.php
@@ -45,6 +45,9 @@
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
 (function (d, w, c) {
